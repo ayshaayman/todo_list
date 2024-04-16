bca75286f973e848c4c0ed517ee2405ae206c2e3
ayshayman <ayman.ayshawasim@gmail.com>
2024-04-17 00:03:13 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todo = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "input",
+            message: "what you want to add in your TODO list?"
+        },
+        {
+            name: "addmore",
+            type: "confirm",
+            message: "do you want to add more?",
+            default: "true"
+        }
+    ]);
+    todo.push(addTask.todo);
+    console.log(todo);
+    condition = addTask.addmore;
+}
